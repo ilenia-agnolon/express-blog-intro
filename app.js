12feb231ba77df6e69a41089d204f581885cd844
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-10-15 17:04:21 +0200

    creato un array dove inserire una lista di almeno 5 post - per ognuno indicato titolo, contenuto, immagine e tags (tags è un array di stringhe) -

--- app.js
+++ app.js
@@ -4,6 +4,42 @@ const express = require("express");
 // creo un'istanza di Express
 const app = express();
 
+//____________________________________________________
+//LISTA POST DEL BLOG
+const posts = [
+  {
+    title: "Ciambellone della domenica",
+    content: "Soffice, profumato e perfetto per la colazione con il latte.",
+    image: "/img/ciambellone.jpeg",
+    tags: ["colazione", "dolci", "tradizione"],
+  },
+  {
+    title: "Cracker alla barbabietola",
+    content: "Snack croccante e coloratissimo, ottimo con hummus o formaggi.",
+    image: "/img/cracker_barbabietola.jpeg",
+    tags: ["snack", "salati", "barbabietola"],
+  },
+  {
+    title: "Pane fritto dolce",
+    content: "Ricetta povera ma irresistibile, zucchero e cannella a pioggia.",
+    image: "/img/pane_fritto_dolce.jpeg",
+    tags: ["dolci", "frittura", "veloci"],
+  },
+  {
+    title: "Pasta alla barbabietola",
+    content: "Cremosa e vivace, con ricotta e noci per un tocco crunchy.",
+    image: "/img/pasta_barbabietola.jpeg",
+    tags: ["primi", "barbabietola", "cremoso"],
+  },
+  {
+    title: "Torta paesana",
+    content: "Dolce lombardo al pane raffermo, cacao e pinoli. Comfort food.",
+    image: "/img/torta_paesana.jpeg",
+    tags: ["dolci", "tradizione", "riciclo"],
+  },
+];
+//____________________________________________________
+
 // imposto una porta di riferimento
 const port = 3000;
 
@@ -12,7 +48,7 @@ app.use(express.static("public"));
 
 // imposto la rotta di index
 app.get("/", (req, res) => {
-  res.send("<h1>Server del mio blog </h1>");
+  res.send("Server del mio blog");
 });
 
 // metto in ascolto il server sulla porta definita
